1d947443e073ab968202ab85a489fbae062b7614
Daniel Aragão Abreu Filho <danielfilhoce@gmail.com>
2018-11-01 01:10:51 -0300

    up

--- src/main/java/com/sample/Patient.java
+++ src/main/java/com/sample/Patient.java
@@ -11,4 +11,39 @@ public class Patient {
 		Symptons = new ArrayList<Symptom>();
 		EtiologyFactors = new ArrayList<EEtiology>();
 	}
+	
+	public boolean hasSymptom(ESymptomType symptom) {
+		for(Symptom s : Symptons) {			
+			if(s.getSymptonType() == symptom) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public List<Symptom> getSymptom(ESymptomType symptom) {
+		List<Symptom> symptons = new ArrayList<Symptom>();
+		
+		for(Symptom s : Symptons) {			
+			if(s.getSymptonType() == symptom) {
+				symptons.add(s);
+			}
+		}
+		
+		return symptons;
+	}
+	
+	public int getMonthsOfSymptom(ESymptomType symptom) {
+		List<Symptom> symptons = getSymptom(symptom);
+		int months = 0;
+		
+		for(Symptom s : symptons) {			
+			if(s.getMonths() > months) {
+				months = s.getMonths();
+			}
+		}
+		
+		return months;
+	}
 }
